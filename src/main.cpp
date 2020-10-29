499c686e6f4a91a40a105e1d5af45c235127ce71
CHENYang <chenyang@CHENYangdeiMac-Pro.local>
2020-10-29 10:34:20 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Autor: CHENYang
+ * @Date: 2020-10-28 19:34:00
+ * @FilePath: /test/helloworld.cpp
+ * @LastEditors: CHENYang
+ * @LastEditTime: 2020-10-28 21:15:11
+ */
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "hello World!\n"; 
+}
